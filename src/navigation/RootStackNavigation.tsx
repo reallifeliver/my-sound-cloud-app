b08967e8a02de638b0c3dc2389fde33d0da41ba9
reallifeliver <pms3300@gmail.com>
2021-04-09 01:17:30 +0900

    file move

--- src/navigation/RootStackNavigation.tsx
+++ src/navigation/RootStackNavigation.tsx
@@ -5,24 +5,32 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AlbumScreen from '../screens/AlbumScreen';
 import MainTabNavigation from './MainTabNavigation';
 import { RootStackParamList } from '../types/navigation';
-
+import ErrorModal from '../components/common/ErrorModal';
+import { useSelector, useDispatch } from 'react-redux';
+import { errorSelector, errorActions } from '../slice/errorSlice';
 const RootStack = createStackNavigator<RootStackParamList>();
 
 export const RootStackNavigation = () => {
+  const error = useSelector(errorSelector);
+  const dispatch = useDispatch();
+  const onCloseError = () => dispatch(errorActions.resetError());
   return (
-    <NavigationContainer>
-      <RootStack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: colors.PRIMARY,
-          },
-          headerTintColor: colors.G_0,
-        }}
-      >
-        <RootStack.Screen name='Main' component={MainTabNavigation} />
-        <RootStack.Screen name='Album' component={AlbumScreen} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <RootStack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.PRIMARY,
+            },
+            headerTintColor: colors.G_0,
+          }}
+        >
+          <RootStack.Screen name='Main' component={MainTabNavigation} />
+          <RootStack.Screen name='Album' component={AlbumScreen} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+      <ErrorModal error={error} onClose={onCloseError} />
+    </>
   );
 };
 
